7f113c3c1731508e73967b51cf7650c40dae8db9
ChrisMitov <mitov.christian@gmail.com>
2015-12-14 09:53:55 +0200

    Game TicTacToe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -60,18 +60,14 @@ public class TicTacToe {
 	}
 
 	public static int hasWon(int[][] board) {
-		if ((board[0][0] == board[0][1]) && (board[0][1] == board[0][2]))
-			return board[0][0];
-		if ((board[1][0] == board[1][1]) && (board[1][1] == board[1][2]))
-			return board[1][0];
-		if ((board[2][0] == board[2][1]) && (board[2][1] == board[2][2]))
-			return board[2][0];
-		if ((board[0][0] == board[1][0]) && (board[1][0] == board[2][0]))
-			return board[0][0];
-		if ((board[0][1] == board[1][1]) && (board[1][1] == board[2][1]))
-			return board[0][1];
-		if ((board[0][2] == board[1][2]) && (board[1][2] == board[2][2]))
-			return board[0][2];
+		for (int i = 0; i < board.length; i++) {
+			if(board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+				return board[i][0];
+			}
+			if(board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+				return board[0][i];
+			}
+		}
 		if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]))
 			return board[0][0];
 		if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]))
